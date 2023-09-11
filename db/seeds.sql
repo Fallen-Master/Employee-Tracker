8754eaffea692db90b303e3ed981409a2f81c009
Rafael Gomez <rgomez2014@gmail.com>
2023-09-10 19:28:16 -0500

    added more seed information

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,7 +14,8 @@ INSERT INTO role (title, salary, department_id) VALUES
 ('Sales Manager', 70000, 5);      
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-('John', 'Doe', 1, NULL);
+('John', 'Doe', 1, NULL),
+('Maria', 'Gonzlez', 2, null);
 
 COMMIT;
 
@@ -22,4 +23,4 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('Mike', 'Chan', 2, 1),
 ('Ashley', 'Rodrguez', 2, 1),
 ('Kevin', 'Tupik', 3, 1),
-('Kunal', 'Singh', 4, 1);
+('Kunal', 'Singh', 4, 2);
